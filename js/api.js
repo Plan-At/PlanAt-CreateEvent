de80da4694a80a5eb619d761d29ea0d82e750c12
scott-jack <90592684+scott-jack@users.noreply.github.com>
2022-06-11 17:41:03 -0700

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,125 @@
+import {getCookie} from "./cookies.js"; // need put full filepath since this is not a node module
+
+//get all event ids, then run the callback
+export function getIDs(callback) {
+    get("https://api.752628.xyz/v2/calendar/event/index", callback);
+}
+//get the events from an array of IDs, then run the callback
+export function getEvents(events, callback) {
+    var url = "https://api.752628.xyz/v2/calendar/event/get?"
+    events.forEach(i => url += ("event_id_list=" + parseInt(i) + "&"));
+    console.log(url);
+    get(url, callback);
+}
+// internal function to handle xhr requests
+function xhrWrapper(method, url, message, callback){
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("pa-token", getToken());
+  if(callback != null) xhr.onload = () => callback(JSON.parse(xhr.responseText));
+  console.log(message);
+  xhr.send(message);
+
+}
+
+//get requests
+export function get(url, callback) {
+  xhrWrapper("GET", url, null, callback);
+}
+//post requests
+export function post(url, message, callback) {
+  xhrWrapper("POST", url,  message, callback);
+}
+
+//update existing event from the event object
+export function updateEvent(user, event, callback){
+  post("https://api.752628.xyz/v2/calendar/event/edit?event_id="+event.id, eventJSON(user, event.name, event.description, event.startTime.valueOf()/1000, event.endTime.valueOf()/1000), callback);
+}
+// Edits an already existing event with new information
+export function editEvent(user, eventId, eventName, eventDesc, start, end, callback){
+  post("https://api.752628.xyz/v2/calendar/event/edit?event_id="+eventId, eventJSON(user, eventName, eventDesc, start, end), callback);
+}
+//create a new event
+export function newEvent(user, eventName, eventDesc, start, end, callback){
+    post("https://api.752628.xyz/v2/calendar/event/create", eventJSON(user, eventName, eventDesc, start, end), callback);
+}
+//delete an event
+export function deleteEvent(event, callback){
+  post("https://api.752628.xyz/v2/calendar/event/delete?event_id="+event.id, "", callback);
+}
+
+//delete an event by an ID
+export function deleteEventById(id, callback){
+  post("https://api.752628.xyz/v2/calendar/event/delete?event_id="+id, callback);
+}
+
+//generate json for an event
+export function eventJSON(user, eventName, eventDesc, start, end){
+  return JSON.stringify({
+    "access_control_list": [
+      {
+        "canonical_name": "public",
+        "person_id": user+"",
+        "permission_list": [
+          "read_full",
+          "edit_full",
+          "delete"
+        ]
+      }
+    ],
+    "display_name": eventName,
+    "description": eventDesc,
+    "start_time": {
+      "text": "string",
+      "timestamp_int": start,
+      "timezone_name": "string",
+      "timezone_offset": 0
+    },
+    "end_time": {
+      "text": "string",
+      "timestamp_int": end,
+      "timezone_name": "string",
+      "timezone_offset": 0
+    },
+    "type_list": [
+      {
+        "type_id": "string",
+        "display_name": "string"
+      }
+    ],
+    "tag_list": [
+      {
+        "tag_id": "string",
+        "display_name": "string"
+      }
+    ]
+  });
+}
+//user id stuff
+var userId;
+//get the user id from a token
+export async function getUserId(){
+  if(userId == null){
+    get("https://api.752628.xyz/v2/user/id/get", json => userId = json.person_id);
+    await sleep(500);
+  }
+  console.log(userId);
+  return userId;
+    // return "1234567890";  
+}
+//token stuff
+export function getToken(){
+  // If return something null might cause 422 error when sending the request
+  const potentialToken = getCookie("pa-token");
+  if (potentialToken != null) {
+    return potentialToken;
+  }
+  return "";
+}
+
+//sleep for ms
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
---
+++ js/calendar.js
@@ -0,0 +1,204 @@
+import { div, btn, id, type, drag, body, value, text, extraClass } from './htmlutilities.js'; // Or the extension could be just `.js`
+import { Time } from './timeutilities.js';
+import { Event } from './event.js';
+import './createevent.js';
+import './manageevent.js';
+import { getEvents, getIDs, getUserId, updateEvent } from './api.js';
+import {setCookie} from "./cookies.js";
+
+// get token from URL parameter
+const up = new URLSearchParams(window.location.search); //URLSearchParams() need the search attribute of window.location
+if (up.get("pa-token") != null && up.get("pa-token") !== ""){
+    console.log("token might presented in URL parameter");
+    console.log(`pa-token: ${up.get("pa-token")}`);
+    if (up.get("pa-token").length === 8){
+        setCookie("pa-token", up.get("pa-token"), 365);
+        window.location = "index.html"; // remove all url parameter so the token not getting leaked
+    }
+    else {
+        console.log("pa-token length not matched");
+    }
+}
+
+//root calendar class that manages everything
+
+//make draggables work
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+//root div 
+const calendar = document.getElementById("calendar");
+
+var e2 = div("row");
+
+const events = new Map();
+//strings for days of week
+const daysOfWeek = ['Sunday', 'Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//variable for the currently grabbed element
+var grab = null;
+//get the user id
+var user = await getUserId();
+
+for (var i = 0; i < 8; i++) {
+    var e3 = div("col", drag(false));
+
+    for (var j = 0; j < 25; j++) {
+        if(j == 0 && i != 0){
+            e3.appendChild(div("box", extraClass("boxTop",(i-1)==new Date().getDay() ? "now" : "notNow"), id((i-1)==new Date().getDay() ? "dated" : ""), text(daysOfWeek[i-1])));
+        } else if (i == 0 && j != 0) {
+            e3.appendChild(div("box", text(new Time(j-1, 0).format())));
+        } else {
+ 
+            var f = div("box", id("box"+(24*(i-1)+j-1)));
+
+
+            // }
+            f.addEventListener('dragenter', dragEnter)
+            f.addEventListener('dragover', dragOver);
+            f.addEventListener('dragleave', dragLeave);
+            f.addEventListener('drop', drop);
+            e3.appendChild(f);
+        }
+        e2.appendChild(e3);
+    }
+
+}
+
+
+
+const wkNow = parseInt((Date.now()/(1000*60*60*24)+10)/7);
+var week = wkNow;
+document.getElementById("myweek").value = (1970+parseInt(week/(365.25/7))+"-W"+parseInt(week%(365.25/7)));
+
+//used to recursivley 
+function boxDown(box) {
+    let str = box.id;
+    return document.getElementById("box"+(parseInt(str.substring(3))+1));
+}
+//update the displayed calendar to the proper events
+export function updateEntries() {
+    events.forEach((ev, dv)=>dv.firstChild.remove());
+    events.clear();
+    //callback hell AAAAAAAAAAAAAAAAAAA
+    getIDs(json => {
+        console.log(json);
+        getEvents(json.event_id_list, json2 => {
+            console.log(json2);
+            json2.result.forEach(info => {
+                // console.log(info);
+                for (let v of events.values()) {
+                    // console.log(v.content + "  " + info.event_id);
+                    if (v.id == info.event_id) {
+                        // console.log("match");
+                        return;
+                    }
+                }
+
+                var start = new Date(info.start_time.timestamp_int * 1000);
+                var end = new Date(info.end_time.timestamp_int * 1000);
+                var iddd = "box" + parseInt(start.getHours()+start.getDay()*24);
+                console.log(iddd);
+                var f = document.getElementById(iddd);
+                console.log(parseInt((start.getTime()/(1000*60*60*24)+11)/7));
+                if (events.get(f) != null || f == null || parseInt((start.getTime()/(1000*60*60*24)+11)/7) != parseInt(week)) return;
+                
+                
+                // console.log(end.getHours());
+                var ev = new Event(start, end, info.event_id, info.display_name, info.description);
+                var over = ev.overview;
+                over.addEventListener('dragstart', dragStart);
+                over.addEventListener('dragend', dragEnd);
+                // console.log(over.id);
+                f.appendChild(over);
+                events.set(f, ev);
+            });
+        });
+    });
+}
+
+updateEntries();
+
+calendar.appendChild(e2);
+
+// Refreshes calendar events when the manage events modal is closed
+var submit2 = document.getElementById("createEvent");
+submit2.addEventListener('hide.bs.modal', updateEntries);
+var submit3 = document.getElementById("manageEventsClose1");
+submit3.addEventListener('click', updateEntries);
+var submit4 = document.getElementById("manageEventsClose2");
+submit4.addEventListener('click', updateEntries);
+
+
+var weekSelector = document.getElementById("myweek");
+weekSelector.addEventListener('input', ()=>{
+    var year = parseInt(weekSelector.value.substring(0, 4));
+    var wk = parseInt(weekSelector.value.substring(6));
+    week = (year-1970)*(365.25/7)+wk+0.8;
+    console.log(week);
+    if(wkNow == parseInt(week)) document.getElementById("dated").classList.replace("notNow", "now");
+    else document.getElementById("dated").classList.replace("now", "notNow");
+    updateEntries();
+})
+function dragStart(e) {
+    grab = events.get(e.target.parentNode);
+    // e.preventDefault();
+    // e.dataTransfer.setData('text', e.target.id);
+    setTimeout(()=> e.target.style.display = 'none', 9);
+}
+
+function dragEnd(e) {
+    e.target.style.display = 'block';
+}
+
+
+
+function dragEnter(e) {
+    if (e.target.classList.contains("box")) {
+        e.target.classList.add('drag-over');
+    }
+
+}
+
+function dragOver(e) {
+    if (e.target.classList.contains("box") && grab != null) {
+        // console.log(events.get(grab).totalTime());
+        var box = e.target;
+        for (let i = 0; i < grab.totalTime(); i++) {
+            if (box == null ||( box.childElementCount > 0 && box != grab.parentNode)) return;
+            else box = boxDown(box);
+        }
+        
+        e.preventDefault();
+    }
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+    e.preventDefault();
+
+    e.target.classList.remove('drag-over');
+
+
+    if (e.target.classList.contains("box")) {
+        // console.log(ev.startTime);
+        var old = grab.overview.parentNode;
+        grab.timeChange(parseInt(e.target.id.substring(3))-parseInt(old.id.substring(3)));
+        // console.log(ev.startTime);
+        // draggable.parentElement.removeChild(draggable.parentElement.firstChild);
+        events.set(e.target, grab);
+        events.delete(old);
+        e.target.appendChild(grab.overview);
+        updateEvent(user, grab, ()=>{});
+    }
+
+
+//reset
+    grab = null;
+
+}
